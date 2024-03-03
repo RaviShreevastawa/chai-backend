d6c2d3e038163087af78e8620c17bc2935b2146f
Ravi_Shrivastava <143920036+RaviShreevastawa@users.noreply.github.com>
2024-03-03 11:56:06 +0530

    Update video.model.js

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String, //clouding ulr
+            required: true
+        },
+        thumbnail:{
+            type: String, //clouding ulr
+            required: true
+        },
+        title:{
+            type: String, 
+            required: true
+        },
+        description:{
+            type: String, 
+            required: true
+        },
+        duration:{
+            type: Number, //cloudnary url
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    { 
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoSchema)
